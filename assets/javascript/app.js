1493ca821d742a2dc36730b4f605515df033c1ed
jarishaw <jarishaw@gmail.com>
2017-10-15 23:02:48 -0500

    coded html pseudo code js

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+
+//user clicks start button
+//starts the game...trivia form appears 
+//Timer immediately counts down
+	//-start button disappears, timer appears
+//user picks answer to question
+	//-user can only pick one answer
+//if user answers all questions, click done button
+//otherwise when timer is up, the game ends
+//displays the number of incorrect & correct answers, and unanswered
+
+});
